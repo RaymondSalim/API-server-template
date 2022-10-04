432dfe2e08eabb83e17165fae9c7cf8080359b6d
Raymond Salim <59678005+RaymondSalim@users.noreply.github.com>
2022-10-04 22:03:53 +0700

    feat(clients): initial upload

---
+++ server/clients/other-microservice/client.go
@@ -0,0 +1,7 @@
+package other_microservice
+
+/*
+	The clients package contains clients that can interact with other services in the platform over HTTP.
+	A client contains an interface describing its functionality, so it can be mocked for tests.
+	The implementation of the interface is in the same file.
+*/
