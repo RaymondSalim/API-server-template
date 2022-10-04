29983ef0e6f357d45f1fd17e8c17efb91bd9d687
Raymond Salim <59678005+RaymondSalim@users.noreply.github.com>
2022-10-04 22:05:20 +0700

    feat(mocks): initial upload

---
+++ server/mocks/mocks.go
@@ -0,0 +1,6 @@
+package mocks
+
+/*
+	The mocks for service and repository are generated using https://github.com/golang/mock
+	Repository requires extra mocking, done with https://github.com/DATA-DOG/go-sqlmock
+*/
