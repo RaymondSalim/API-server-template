8dbb12e894a6038bd0c1567fb64765bbcf0ea365
Raymond Salim <59678005+RaymondSalim@users.noreply.github.com>
2022-10-11 14:10:34 +0700

    feat(hooks): updated setup script to also replace github username & grant executable permissions

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+RED='\033[0;31m'
 GREEN='\033[0;32m'
 BLUE='\033[0;34m'
 YELLOW='\033[0;33m'
@@ -24,6 +25,16 @@ print_reminder() {
   print_green "Initialization successful. Don't forget to setup your register your remote git url"
 }
 
+print_start_warning() {
+  echo -e "Do you wish to run this script? ${RED}This will reinitialize git in this directory${NC}"
+  select yn in "Yes" "No"; do
+      case $yn in
+          Yes ) break;;
+          No ) exit;;
+      esac
+  done
+}
+
 reset_git() {
   rm -rf ./.git
   git init &> /dev/null
@@ -34,6 +45,11 @@ update_hooks_dir() {
   git config core.hooksPath .githooks
 }
 
+grant_executable_perm() {
+  print_yellow "Granting executable permission on hooks script"
+  chmod -R +x .githooks
+}
+
 update_project_name() {
   print_yellow "Enter Project Name"
   read -r proj_name
@@ -41,9 +57,19 @@ update_project_name() {
   grep -rl 'API-server-template' . | xargs sed -i "s/API-server-template/${proj_name}/g"
 }
 
+update_github_username() {
+  print_yellow "Enter GitHub Username"
+  read -r usnm
+
+  grep -rl 'RaymondSalim' . | xargs sed -i "s/RaymondSalim/${usnm}/g"
+}
+
+print_start_warning
 reset_git
 update_hooks_dir
+grant_executable_perm
 update_project_name
+update_github_username
 print_reminder
 
 
