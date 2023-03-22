470b9566cbcee9ef31d55065a0ac8c5ab69f49cc
Raymond Salim <59678005+RaymondSalim@users.noreply.github.com>
2023-03-22 12:43:50 +0700

    feat(model): updated response model

--- server/models/response/response.go
+++ server/models/response/response.go
@@ -4,5 +4,10 @@ type BaseResponse struct {
 	Status     string      `json:"status"`
 	StatusCode int         `json:"statusCode"`
 	RequestID  string      `json:"requestID"`
-	Payload    interface{} `json:"payload"`
+	Payload    interface{} `json:"payload,omitempty"`
+}
+
+type APIFormError struct {
+	Field string `json:"field"`
+	Error string `json:"error"`
 }
